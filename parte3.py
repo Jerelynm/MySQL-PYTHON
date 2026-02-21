6138cfdf599f08861fbd11bb4b246bfd9a79739b
Jerelynm <jmarinm4@miumg.edu.gt>
2026-02-20 22:28:17 -0600

    Modificado para que sea funcional

--- parte3.py
+++ parte3.py
@@ -1,26 +1,25 @@
 import mysql.connector
 from mysql.connector import Error
 
+conexion = None
 try:
     conexion = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root",
-        database="tarea1"
-    )
-
+        host='localhost',
+        user='root',
+        password='root',
+        database='tarea1')
     if conexion.is_connected():
         print("Conexión exitosa")
         cursor = conexion.cursor()
         cursor.execute("SELECT DATABASE();")
-        db=cursor.fetchone()
-        print("Base de datos: {db}")
+        db = cursor.fetchone()
+        print(f"Base de datos actual: {db[0]}")
 
 except Error as e:
-    print(f"Error: {e}")
+    print(f"Error al conectar a MySQL: {e}")
 
 finally:
-    if conexion.is_connected():
+    if conexion is not None and conexion.is_connected():
         cursor.close()
         conexion.close()
         print("Conexión cerrada")
